4d788f75911ade5f5f3fa60a3ccb137fa7091fe0
kairadiant <98728635+kairadiant@users.noreply.github.com>
2022-09-15 02:47:10 -0400

    Add files via upload

---
+++ src/main/java/com/example/wordle/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.example.wordle
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+     var guessesleft = 3
+
+    private fun play{
+
+        while (guessesleft > 0){
+
+        }
+    }
+
+    private fun checkGuess(guess: String) : String {
+        var result = ""
+        for (i in 0..3) {
+            if (guess[i] == wordToGuess[i]) {
+                result += "O"
+            }
+            else if (guess[i] in wordToGuess) {
+                result += "+"
+            }
+            else {
+                result += "X"
+            }
+        }
+        return result
+    }
+
+
+}
